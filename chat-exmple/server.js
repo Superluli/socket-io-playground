6d9f1dc2f206fce1f9f8f446a5eabf4e80d62dcc
Lu Li <lu3.li@samsung.com>
2014-11-10 18:02:37 -0800

    x

--- chat-exmple/server.js
+++ chat-exmple/server.js
@@ -2,19 +2,39 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var id = 0;
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+var users = {};
+
 io.on('connection', function(socket){
-  console.log('a user connected');
+  
+  var name = "user" + (id ++); 
+  users[name] = 1;
+
+  io.emit('action message', name + " has joined");
+
+  io.emit('id message', name);  
+
+  io.emit('online message', users);
+
   socket.on('disconnect', function(){
-    console.log('user disconnected');
+    io.emit('action message', name + " has left");
+    delete users[name];
+    io.emit('online message', users);
   });
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
+
+  socket.on('chat message', function(text){
+    io.emit('chat message', {id : name, msg : name + " : " + text});
   });
+
+  socket.on('typing message', function(){
+    io.emit('typing message', {id : name, msg : name + " is typing..."});
+  });
+
 });
 
 http.listen(3000, function(){
